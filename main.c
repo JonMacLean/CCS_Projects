72e07fdd12936e33c4d7bf1531fd301a8903fd60
Jon MacLean <jdmaclean6@gmail.com>
2014-07-14 15:50:08 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 
 ///*********************************************************************
-//|                         1._Documentation_
+//|                         1ntation_
 //|
 //|  FILE:            Main.c
 //|
@@ -93,7 +93,7 @@
 //|
 //|  BACKGROUND: _______________________________________________________
 //|                The definitions map the [Pin] number to the labeling
-//|              provided in the datasheet for PCD8544 48 × 84 pixels
+//|              provided in the datasheet for PCD8544 48 ï¿½ 84 pixels
 //|              matrix LCD controller/driver.
 //|
 //|
